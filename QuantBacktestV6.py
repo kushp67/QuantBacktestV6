6a39fa68c326695ac9a4031078c956d806af5ec5
kushp67 <41266792+kushp67@users.noreply.github.com>
2025-02-02 14:02:42 -0500

    Update QuantBacktestV6.py

--- QuantBacktestV6.py
+++ QuantBacktestV6.py
@@ -748,7 +748,7 @@ def main():
 
         st.markdown("---")
        
-                st.subheader("Strategy vs. Buy & Hold Comparison")
+        st.subheader("Strategy vs. Buy & Hold Comparison")
         fig2 = plot_buy_hold_comparison(portfolio, data, initial_capital)
         st.pyplot(fig2)
 
